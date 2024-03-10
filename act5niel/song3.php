1811b1a5889e65881616c08208e21c82a0f33b90
OriaselAaron <oriasel.aaron@mscmarinduque.edu.ph>
2024-03-10 16:38:02 +0800

    song3.php added

---
+++ act5niel/song3.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>San Francisco Street - Chords and Lyrics</title>
+</head>
+<style>
+        body {
+            font-family: 'verdana', 'Arial';
+            background-color: cyan; 
+            color: #333;
+            margin: 0;
+            padding: 20px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+        }
+        h1 {
+            font-size: 3em;
+            color: #000000;
+            margin-bottom: 30px;
+            margin-top: 1000px;
+
+        }
+        pre {
+            white-space: pre-wrap;
+            font-family: 'Helvetica', 'Helvetica';
+            font-size: 1em;
+            font-weight: bold;
+            color: #000000;
+            margin: auto;
+        }
+        .back {
+            text-decoration: none;
+            font-family: 'verdana', 'Arial';
+            color: black;
+        }
+    </style>
+<body>
+
+<?php
+echo "<h1>San Francisco Street - Chords and Lyrics</h1>\n";
+echo "<pre>\n";
+
+echo "[Intro]\n";
+echo "Dmaj7 Gmaj7 Em A\n";
+echo "Dmaj7 Gmaj7 Em A\n";
+echo "Dmaj7 Dmaj7 Em Em\n\n";
+
+echo "[Verse 1]\n";
+echo "Dmaj7                 Em\n";
+echo "Lost on the freeway Well, this must be LA\n";
+echo "Dmaj7             Em\n";
+echo "Tired and lonely With no real place to stay\n";
+echo "Dmaj7                  Em\n";
+echo "Is this the future and where I'm gonna be?\n";
+echo "F#m                          Gmaj7           A\n";
+echo "Last night it hit me When I had this crazy dream\n\n";
+
+echo "[Chorus]\n";
+echo "           Dmaj7       Gmaj7\n";
+echo "Of waking up In your house\n";
+echo "          Em        A\n";
+echo "On a San Francisco street\n";
+echo "        Dmaj7                   Gmaj7\n";
+echo "We tune out all the nasty weather\n";
+echo "         Em                      A\n";
+echo "And it's all in front of you and me\n\n";
+
+echo "[Verse 2]\n";
+echo "Dmaj7                     Em\n";
+echo "You were intriguing Just looking at a magazine\n";
+echo "Dmaj7                     Em\n";
+echo "Like when I first saw you Back in two thousand and three\n";
+echo "Dmaj7                            Em\n";
+echo "Then you walked over and Said: \"you don't have to be alone.\"\n";
+echo "F#m                              Gmaj7.                        A\n";
+echo "I don't have a crystal ball... There's no way that I could've known\n\n";
+
+echo "[Chorus]\n";
+echo "                Dmaj7          Gmaj7\n";
+echo "(I'd be) waking up In your house\n";
+echo "     Em            A\n";
+echo "On a San Francisco street\n";
+echo "        Dmaj7                   Gmaj7\n";
+echo "We tune out all the nasty weather\n";
+echo "         Em                      A\n";
+echo "And it's all in front of you and me\n";
+echo "       Dmaj7          Gmaj7\n";
+echo "Waking up In your house\n";
+echo "     Em             A\n";
+echo "On a San Francisco street\n";
+echo "        Dmaj7                 Gmaj7\n";
+echo "We tune out all the nasty weather\n";
+echo "         Em\n";
+echo "And it's all in front of you\n";
+echo "A                      Bb C         Dmaj7\n";
+echo "All in front of you and me   oh, oh\n\n";
+
+echo "[Interlude]\n";
+echo "Bb C Dmaj7 Dmaj7\n";
+echo "Bb C Dmaj7 Dmaj7\n";
+echo "Bb C Dmaj7 Dmaj7\n";
+echo "Bb C Dmaj7 Dmaj7\n";
+echo "Yeah...\n\n";
+
+echo "[Chorus]\n";
+echo "       Dmaj7        Gmaj7\n";
+echo "Waking up In your house\n";
+echo "     Em             A\n";
+echo "On a San Francisco street\n";
+echo "        Dmaj7              Gmaj7\n";
+echo "We tune out all the nasty weather\n";
+echo "          Em                     A\n";
+echo "And it's all in front of you and me\n\n";
+
+echo "[Chorus]\n";
+echo "       Dmaj7        Gmaj7\n";
+echo "Waking up In your house\n";
+echo "      Em            A\n";
+echo "On a San Francisco street\n";
+echo "         Dmaj7             Gmaj7\n";
+echo "We tune out all the nasty weather\n";
+echo "          Em\n";
+echo "Then it's all in front of you\n";
+echo "A                        Dmaj7 Gmaj7\n";
+echo "All in front of you and me\n\n";
+
+echo "[Outro]\n";
+echo "Em            A\n";
+echo "San Francisco street\n";
+echo "        Dmaj7 Gmaj7\n";
+echo "You and me\n";
+echo "Em                A\n";
+echo "So only front of you, yeah\n";
+echo "        Dmaj7\n";
+echo "You and me\n";
+echo "Em             A              Dmaj7 Gmaj7\n";
+echo "San Francisco street, you and me\n";
+echo "     Em Gmaj7 Dmaj7\n";
+echo "Oh, oh\n";
+
+echo "</pre>\n";
+
+?>
+
+<br>
+<div>
+    <a class="back" href="index.php">>>> Go Back <<<</a>
+</div>
+</body>
+</html>
