42e16459d5256d6e6f8cb2be87bfb82558bfa78e
OriaselAaron <oriasel.aaron@mscmarinduque.edu.ph>
2024-03-10 16:38:44 +0800

    song4.php added

---
+++ act5niel/song4.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Heart Can't Lose - Chords and Lyrics</title>
+</head>
+<style>
+        body {
+            font-family: 'verdana', 'Arial';
+            background-color: plum; 
+            color: #333;
+            margin: 0;
+            padding: 20px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+        }
+        h1 {
+            font-size: 3em;
+            color: #000000;
+            margin-bottom: 30px;
+            margin-top: 2000px;
+
+        }
+        pre {
+            white-space: pre-wrap;
+            font-family: 'Helvetica', 'Helvetica';
+            font-size: 1em;
+            font-weight: bold;
+            color: #000000;
+            margin: auto;
+        }
+        .back {
+            text-decoration: none;
+            font-family: 'verdana', 'Arial';
+            color: black;
+        }
+    </style>
+<body>
+
+<?php
+echo "<h1>Heart Can't Lose - Chords and Lyrics</h1>\n";
+echo "<pre>\n";
+
+echo "[Intro]\n";
+echo "Db    Bbm    Ab    Gb\n";
+echo "Db    Bbm    Ab    Gb\n\n";
+
+echo "[Verse 1]\n";
+echo "Db             Bbm            Ab\n";
+echo "  I don't know how I ended up\n";
+echo "             Gb\n";
+echo "In a situation where I feel my heart\n";
+echo "Db           Bbm          Ab        Gb\n";
+echo "I feel it beating when I see you\n";
+echo "Db           Bbm                Ab\n";
+echo "I don't know how you do it to me\n";
+echo "         Gb               Db\n";
+echo "But every time I see you walking around\n";
+echo "Bbm                Ab          Gb\n";
+echo "It's like everything slows down\n\n";
+
+echo "[Pre-Chorus]\n";
+echo "  Ab        Bbm         Gb\n";
+echo "I hated love but then I met you\n\n";
+
+echo "[Chorus]\n";
+echo "                        Db\n";
+echo "What the hell did you do to me\n";
+echo "    Bbm             Gb\n";
+echo "I'm feeling love again\n\n";
+echo "I can't believe that\n";
+echo "   Db               Bbm\n";
+echo "I broke the promise to myself\n";
+echo "     Gb\n";
+echo "That I won't fall again\n";
+echo "               Ab\n";
+echo "My heart is a fool\n";
+echo "                  Bbm\n";
+echo "And I don't wanna lose\n";
+echo "             Gb\n";
+echo "But it keeps doing the things you want it to do\n";
+echo "     Db                 Bbm              Gb\n";
+echo "It's hard to admit that I'm in love with you\n";
+echo "                              Db        Bbm\n";
+echo "I want you but my heart can't lose\n";
+echo "        Gb\n";
+echo "Na, na, na\n\n";
+
+echo "[Verse 2]\n";
+echo "Db              Bbm\n";
+echo "  I might sound crazy\n";
+echo "                Ab\n";
+echo "But I wanna die\n";
+echo "           Gb\n";
+echo "Every time I look into your eyes\n";
+echo "Db          Bbm            Ab       Gb\n";
+echo "  Oh baby I want you to be mine\n";
+echo "   Db                      Bbm\n";
+echo "I still don't know how you did it\n";
+echo "       Ab               Gb\n";
+echo "Oh my heart was tired of getting broken\n";
+echo "   Db            Bbm                     Ab       Gb\n";
+echo "I let it frown,  but now it's hunting me down\n\n";
+
+echo "[Pre-Chorus]\n";
+echo "  Ab        Bbm         Gb\n";
+echo "I hated love but then I met you\n\n";
+
+echo "[Chorus]\n";
+echo "                        Db\n";
+echo "What the hell did you do to me\n";
+echo "    Bbm             Gb\n";
+echo "I'm feeling love again\n\n";
+echo "I can't believe that\n";
+echo "   Db               Bbm\n";
+echo "I broke the promise to myself\n";
+echo "     Gb\n";
+echo "That I won't fall again\n";
+echo "               Ab\n";
+echo "My heart is a fool\n";
+echo "                  Bbm\n";
+echo "And I don't wanna lose\n";
+echo "             Gb\n";
+echo "But it keeps doing the things you want it to do\n";
+echo "     Db                 Bbm              Gb\n";
+echo "It's hard to admit that I'm in love with you\n";
+echo "                              Db        Bbm\n";
+echo "I want you but my heart can't lose\n";
+echo "           Gb\n";
+echo "I want you now\n\n";
+
+echo "[Bridge]\n";
+echo "  Db                          Bbm\n";
+echo "I want you but my heart can't lose\n";
+echo "  Gb\n";
+echo "I want you but my heart can't lose\n";
+echo "  Db                          Bbm\n";
+echo "I want you but my heart can't lose\n";
+echo "  Gb\n";
+echo "I want you but my heart can't lose\n";
+echo "  Db                          Bbm\n";
+echo "I want you but my heart can't lose\n";
+echo "  Gb\n";
+echo "I love you but my heart can't lose\n\n";
+
+echo "[Chorus]\n";
+echo "                        Db\n";
+echo "What the hell did you do to me\n";
+echo "    Bbm             Gb\n";
+echo "I'm feeling love again\n\n";
+echo "I can't believe that\n";
+echo "   Db               Bbm\n";
+echo "I broke the promise to myself\n";
+echo "     Gb\n";
+echo "That I won't fall again\n";
+echo "               Ab\n";
+echo "My heart is a fool\n";
+echo "                  Bbm\n";
+echo "And I don't wanna lose\n";
+echo "             Gb                   GbMaj7\n";
+echo "But it keeps doing the things you want it to do\n";
+echo "     Db                 Bbm              Gb\n";
+echo "It's hard to admit that I'm in love with you\n";
+echo "  GbMaj7                      Db        Bbm\n";
+echo "I want you but my heart can't lose\n";
+echo "Gb          GbMaj7      Ab\n";
+echo "Yeah, yeah, yeah\n";
+echo "    Bbm             Gb\n";
+echo "Na, na, na, na, na, na\n";
+
+echo "</pre>\n";
+
+?>
+
+<br>
+<div>
+    <a class="back" href="index.php">>>> Go Back <<<</a>
+</div>
+</body>
+</html>
