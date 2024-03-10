d8aa0ea85b3e659641891c61b8b35904309661ad
OriaselAaron <oriasel.aaron@mscmarinduque.edu.ph>
2024-03-10 16:37:07 +0800

    song2.php added

---
+++ act5niel/song2.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Para sa akin - Chords and Lyrics</title>
+</head>
+<style>
+        body {
+            font-family: 'verdana', 'Arial';
+            background-color: orange; 
+            color: #333;
+            margin: 0;
+            padding: 20px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+        }
+        h1 {
+            font-size: 3em;
+            color: #000000;
+            margin-bottom: 30px;
+            margin-top: 1000px;
+
+        }
+        pre {
+            white-space: pre-wrap;
+            font-family: 'Helvetica', 'Helvetica';
+            font-size: 1em;
+            font-weight: bold;
+            color: #000000;
+            margin: auto;
+        }
+        .back {
+            text-decoration: none;
+            font-family: 'verdana', 'Arial';
+            color: black;
+        }
+    </style>
+<body>
+
+<?php
+echo "<h1>Para sa akin - Chords and Lyrics</h1>\n";
+echo "<pre>\n";
+
+echo "[Intro]\n";
+echo "D C#m7 F#7 Bm D7 G A7 break\n\n";
+
+echo "[Verse]\n";
+echo "  D                    C#m7 F#7\n";
+echo "   Kung ika'y magiging akin\n";
+echo "  Bm               D7\n";
+echo "   Di ka na muling luluha pa\n";
+echo "       G               F#m  B7\n";
+echo "   Pangakong di ka lolokohin\n";
+echo "      Em     Em7      A7\n";
+echo "   Ng puso kong nagmamahal\n";
+echo "  D                C#m7 F#7\n";
+echo "   Kung ako ay papalarin\n";
+echo "  Bm               D7\n";
+echo "   Na ako'y iyong mahal na rin\n";
+echo "       G                  F#m B7\n";
+echo "   Pangakong ikaw lang ang iibigin\n";
+echo "    Em        A7\n";
+echo "   Magpakailanman\n\n";
+
+echo "[Chorus]\n";
+echo "        D      D7\n";
+echo "   Di kita pipilitin\n";
+echo "       G            Gm   C7\n";
+echo "   Sundin mo pang iyong damdamin\n";
+echo "      F#m             B7sus   B7   Em Gm\n";
+echo "   Hayaan na lang tumibok ang puso mo\n";
+echo "     A7      D A7\n";
+echo "   Para sa akin\n\n";
+
+echo "[Verse]\n";
+echo "  D               C#m7 F#7\n";
+echo "   Kung ako ay mamalasin\n";
+echo "  Bm                  D7\n";
+echo "   At mayroon ka nang ibang mahal\n";
+echo "        G              F#m     B7\n";
+echo "   Ngunit patuloy ang aking pag-ibig\n";
+echo "    Em        A7\n";
+echo "   Magpakailanman\n\n";
+
+echo "[Chorus]\n";
+echo "        D      D7\n";
+echo "   Di kita pipilitin\n";
+echo "       G            Gm   C7\n";
+echo "   Sundin mo pang iyong damdamin\n";
+echo "      F#m             B7sus   B7   Em Gm\n";
+echo "   Hayaan na lang tumibok ang puso mo\n";
+echo "     A7       F Em A7\n";
+echo "   Para sa akin\n\n";
+
+echo "[Adlib]\n";
+echo "D CM7 FM7 Em A7 break\n\n";
+
+echo "[Verse]\n";
+echo "  D                C#m7 F#7\n";
+echo "   Kung ako ay papalarin\n";
+echo "  Bm               D7\n";
+echo "   Na ako'y iyong mahal na rin\n";
+echo "       G                  F#m B7\n";
+echo "   Pangakong ikaw lang ang iibigin\n";
+echo "    Em        A7\n";
+echo "   Magpakailanman\n\n";
+
+echo "[Chorus]\n";
+echo "        D      D7\n";
+echo "   Di kita pipilitin\n";
+echo "       G            Gm   C7\n";
+echo "   Sundin mo pang iyong damdamin\n";
+echo "      F#m             B7sus   B7   Em Gm\n";
+echo "   Hayaan na lang tumibok ang puso mo\n";
+echo "     A7         D A7\n";
+echo "   Para sa akin\n\n";
+
+echo "      D      D7\n";
+echo "   Di kita pipilitin\n";
+echo "       G            Gm   C7\n";
+echo "   Sundin mo pang iyong damdamin\n";
+echo "      F#m             B7sus   B7   Em Gm\n";
+echo "   Hayaan na lang tumibok ang puso mo\n";
+echo "     A7          Bm Bm/Bb Bm/A Bm/Ab Em Eb pause\n";
+echo "   Para sa akin\n";
+echo "               D\n";
+echo "   Para sa akin\n\n";
+
+echo("// Mabuhay ang mga STIers\n");
+echo("X");
+
+echo "</pre>\n";
+
+?>
+
+<br>
+<div>
+    <a class="back" href="index.php">>>> Go Back <<<</a>
+</div>
+</body>
+</html>
