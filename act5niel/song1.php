21118b30f87f81ef509a2759c2a15027469777d7
OriaselAaron <oriasel.aaron@mscmarinduque.edu.ph>
2024-03-10 16:35:01 +0800

    song1.php added

---
+++ act5niel/song1.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dancing with my phone - Chords and Lyrics</title>
+</head>
+<style>
+        body {
+            font-family: 'verdana', 'Arial';
+            background-color: #78C481; 
+            color: #333;
+            margin: 0;
+            padding: 20px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+        }
+        h1 {
+            font-size: 3em;
+            color: #000000;
+            margin-bottom: 30px;
+            margin-top: 1000px;
+
+        }
+        pre {
+            white-space: pre-wrap;
+            font-family: 'Helvetica', 'Helvetica';
+            font-size: 1em;
+            font-weight: bold;
+            color: #000000;
+            margin: auto;
+        }
+        .back {
+            text-decoration: none;
+            font-family: 'verdana', 'Arial';
+            color: black;
+        }
+    </style>
+<body>
+
+<?php
+echo "<h1>Dancing with my phone - Chords and Lyrics</h1>\n";
+echo "<pre>\n";
+
+echo "[Intro]\n";
+echo "G#m7  C#  F#maj7   Bmaj7  x2\n\n";
+
+echo "[Verse 1]\n";
+echo "\nG#m7                       C#\n";
+echo "    I’m just laying on the floor again\n";
+echo "F#maj7                     Bmaj7\n";
+echo "  Can’t be bothered to get up now\n";
+echo "           G#m7\n";
+echo "I wouldn’t care\n";
+echo "               C#\n";
+echo "If I never get up again\n";
+echo "        F#maj7     Bmaj7\n";
+echo "I don’t want to\n";
+echo "G#m7                         C#\n";
+echo "  Then our song comes on the radio\n";
+echo "F#maj7                   Bmaj7\n";
+echo "   Makes me wanna start to dance, oh\n";
+echo "       G#m7\n";
+echo "I wanna know\n";
+echo "                    C#\n";
+echo "If you feel the same way as me\n";
+echo "          F#maj7      Bmaj7\n";
+echo "Why would you go?\n\n";
+
+echo "[Chorus]\n";
+echo "G#m7             C#\n";
+echo "Dancing, I’m all alone\n";
+echo "         F#maj7                Bmaj7\n";
+echo "Figuring out how I can get you home\n";
+echo "G#m7            C#\n";
+echo "Dancing with my phone\n";
+echo "         F#maj7     Bmaj7\n";
+echo "Thinking about you\n\n";
+
+echo "[Verse 2]\n";
+echo "G#m7                     C#\n";
+echo "  On my feet and now I’m out the door\n";
+echo "F#maj7                       Bmaj7\n";
+echo "  Walking by the places that we used to go\n";
+echo "  G#m7              C#\n";
+echo "I remember all your favorite stores\n";
+echo "        F#maj7     Bmaj7\n";
+echo "I won’t lie\n\n";
+
+echo "[Breakdown]\n";
+echo "G#m7              C#          F#maj7        Bmaj7\n";
+echo "  I don’t think I even know myself       anymore\n";
+echo "G#m7                 C#              F#maj7\n";
+echo "  You’re the one who knew me fucking well\n";
+echo "          Bmaj7\n";
+echo "Yeah, you know\n\n";
+
+echo "[Chorus]\n";
+echo "G#m7             C#\n";
+echo "Dancing, I’m all alone\n";
+echo "         F#maj7                Bmaj7\n";
+echo "Figuring out how I can get you home\n";
+echo "G#m7            C#\n";
+echo "Dancing with my phone\n";
+echo "         F#maj7     Bmaj7\n";
+echo "Thinking about you\n";
+echo "G#m7             C#\n";
+echo "Dancing, I’m all alone (I'm dancing all alone)\n";
+echo "         F#maj7                Bmaj7\n";
+echo "Figuring out how I can get you home (How I can get you home)\n";
+echo "G#m7            C#\n";
+echo "Dancing with my phone (I'm dancing with my phone)\n";
+echo "         F#maj7     Bmaj7\n";
+echo "Thinking about you\n\n";
+
+echo "[Outro]\n";
+echo "G#m7                       C#\n";
+echo "Dancing all alone, dancing all alone (I'm dancing all alone)\n";
+echo "F#maj7                     Bmaj7\n";
+echo "Dancing all alone, dancing all alone (I'm dancing with my phone)\n";
+echo "G#m7            C#\n";
+echo "Dancing with my phone\n";
+echo "         F#maj7        Bmaj7\n";
+echo "Thinking about you\n";
+echo "G#m7                       C#\n";
+echo "Dancing all alone, dancing all alone\n";
+echo "F#maj7                     Bmaj7\n";
+echo "Dancing all alone, dancing all alone (Thinking about you)\n";
+echo "G#m7            C#\n";
+echo "Dancing with my phone\n";
+echo "         F#maj7      Bmaj7\n";
+echo "Thinking about you\n";
+
+echo "</pre>\n";
+
+?>
+
+<br>
+<div>
+    <a class="back" href="index.php">>>> Go Back <<<</a>
+</div>
+</body>
+</html>
